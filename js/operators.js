da074dd680fcf232f6d9d68ec50cd59ae9ad983a
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-04-08 18:00:59 +0200

    1

--- js/operators.js
+++ js/operators.js
@@ -1,3 +1,5 @@
+// 1 урок
+
 // // Шість значень які призводяться до false:
 // console.log(Boolean());
 // console.log(Boolean(false));
@@ -16,9 +18,9 @@
 // Якщо десь зустрінеться false то воно і зупинеться
 // Якщо всі умови true, то воно повертає останнє значення
 // Якщо два значення false, буде перший false який воно зустріло
-console.log(3 && 2);
-console.log(null && undefined);
-console.log(2 && false);
+// console.log(3 && 2);
+// console.log(null && undefined);
+// console.log(2 && false);
 
 
 // Логічний оператор "або"
@@ -26,8 +28,8 @@ console.log(2 && false);
 // 
 // Якщо є true то буде перший true
 // Якщо всі значення false то буде виводитися останній false
-console.log(0 || 2);
-console.log(null || undefined);
+// console.log(0 || 2);
+// console.log(null || undefined);
 
 
 // Логічний оператор "не"
@@ -68,7 +70,89 @@ console.log(null || undefined);
 // другом 
 // онлайн 
 // без режиму не турбувати 
-const isOnline = false; 
-const isFriend = true; 
-const isDisturb = false; 
-console.log(isOnline && isFriend && isDisturb && "Вітаю, ви можете увійти у систему");
+// const isOnline = false; 
+// const isFriend = true; 
+// const isDisturb = false; 
+// console.log(isOnline && isFriend && isDisturb && "Вітаю, ви можете увійти у систему");
+
+
+// 2 урок
+
+// if (a = a) {  // Умова, яка має призвестись до true
+//     // Тіло умови
+// }
+
+// const number = Number(prompt("Введіть число"));
+// if (number > 0) {
+//     console.log(`Число ${number} більше 0`) // Це буде виводитись якщо число number більше нуля
+// }
+
+// const number = Number(prompt("Введіть число")) 
+// if (number > 18) {
+//     console.log(`Ви можете голосувати!`);
+// }
+
+// const number = Number(prompt("Введіть число"));
+// if (number % 5 === 0) {
+//     console.log(`Число ${number} кратне 5`);
+// }
+
+// const str = prompt("Введіть рядок");
+// if (str.length > 10) {
+//     console.log(`У вашому рядку більше 10 символів`);
+// }
+
+// const number = Number(prompt("Введіть число"));
+// if (number % 2 === 0) {
+//     console.log(`Ваше число є чотним`);
+// }
+
+// const password = "qwerty";
+// const enter = prompt("Введіть пароль");
+// if (enter === password) { // Якщо
+//     console.log(`Ви ввели правильний пароль!`);
+// } else { // Інакше, у нього немає перевірки!
+//     console.log(`Ви ввели неправильний пароль!`);
+// }
+
+// const number = Number(prompt("Введіть число"));
+// if (number > 0) {
+//     console.log(`Число більше 0`);
+// } else {
+//     console.log(`Число менше 0`);
+// }
+
+// const age = Number(prompt(`Введіть свій вік`));
+// if (age > 100) {
+//     console.log(`Ви дуже старі!`);
+// } else if (age > 75) { // Інакше якщо, приймає перевірку
+//     console.log(`Ви старі!`);
+// } else if (age > 50) { // Інакше якщо, приймає перевірку
+//     console.log(`Ви підстаркуваті!`);
+// } else if (age > 30) { // Інакше якщо, приймає перевірку
+//     console.log(`Ви молоді!`);
+// } else {
+//     console.log(`Юний`);
+// }
+
+// const x = Number(prompt("Введіть число по х")); 
+// const y = Number(prompt("Пведіть число по у")); 
+// if (x>0 && y>0) { 
+//     console.log("Перша чверть"); 
+// } else if (x<0 && y>0) { 
+//     console.log("Друга чверть"); 
+// } else if (x<0 && y<0) {
+//     console.log("Третя чверть"); 
+// } else if (x>0 && y<0) {
+//     console.log("4 чверть");
+// }
+
+// const x = Number(prompt("Введіть число по х")); 
+// const y = Number(prompt("Пведіть число по у")); 
+// const op = prompt("Введіть опертаор / * - +");
+// if (op === "/") {
+//     console.log();
+
+// const age = Number(prompt("Ваш вік")) 
+// const result = (age > 18) ? `Ви дорослий, ваш вік ${age}`: `Вам немає 18, ви ввели ${age}`;
+// console.log(result);
